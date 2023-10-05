5d697628f2d4d8d67512b46e2bf15cf25b677590
mylo <36931363+gitmylo@users.noreply.github.com>
2023-10-05 15:25:32 +0200

    TTS extension, has all features audio-webui originally had for TTS

---
+++ requirements.py
@@ -0,0 +1,12 @@
+from setup_tools.magicinstaller.requirement import SimpleRequirement
+
+
+class TTS(SimpleRequirement):
+    package_name = 'TTS'
+
+    def install(self) -> tuple[int, str, str]:
+        return self.install_pip("tts[ja]", "TTS")
+
+
+def requirements():
+    return [TTS()]
